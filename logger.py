e23439bfbe311bf9c8b31d2f7857de622858c7d6
muhammedasadn <asadnasar748@gmail.com>
2026-03-02 21:50:09 +0530

    update: config, logger..py, detector.py

--- logger.py
+++ logger.py
@@ -1,11 +1,15 @@
-# logger.py
-
 import logging
 
-def setup_logger():
-    logging.basicConfig(
-        filename="ssh_detector.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    return logging
+def setup_logger(name="ssh_detector", level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
